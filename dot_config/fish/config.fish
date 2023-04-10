24518551aa21433a4a0e9a8d1b265d7ee81004d8
Wulfre <wulfre@users.noreply.github.com>
2023-04-10 09:08:14 -0400

    add sections for linux-only and mac-only configs

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,16 +1,27 @@
+# Global always-use config
 export STARSHIP_CONFIG=$HOME/.config/starship/starship.toml
 
 if status is-interactive
+    # Linux-only interactive-only config
+    if test $(uname -s) = "Linux"
+        set -gx PNPM_HOME "$HOME/.local/share/pnpm"
+        if not string match -q -- $PNPM_HOME $PATH
+            set -gx PATH "$PNPM_HOME" $PATH
+        end
+    end
+
+    # MacOS-only interactive-only config
+    if test $(uname -s) = "Darwin"
+        set -gx PNPM_HOME "$HOME/Library/pnpm"
+        if not string match -q -- $PNPM_HOME $PATH
+            set -gx PATH "$PNPM_HOME" $PATH
+        end
+    end
+
+    # Global interactive-only config
     fundle plugin "meaningful-ooo/sponge"
     fundle plugin "jethrokuan/z"
     fundle init
 
     starship init fish | source
 end
-
-# pnpm
-set -gx PNPM_HOME "/home/sean/.local/share/pnpm"
-if not string match -q -- $PNPM_HOME $PATH
-  set -gx PATH "$PNPM_HOME" $PATH
-end
-# pnpm end
